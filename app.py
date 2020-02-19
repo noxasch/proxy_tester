9c83acdbe5aa6bc159a15bbd2576c0ab6f3b2307
Alexander Dischberg <noxasch@gmail.com>
2020-02-19 08:42:06 +0800

    Move unittest and add configuration folder

--- app.py
+++ app.py
@@ -4,11 +4,17 @@ import re
 import traceback
 from sys import argv
 
-PROXY = '207.246.78.249:80'
+# PROXY = '207.246.78.249:80'
+# TODO: 
+# - accept custom url
+# - accept custom header
+# - output result to file
+
 PING_URL = 'https://www.google.com'
 HEADER = ''
 
 def test_proxy(proxy = None):
+    print('test proxy')
     if not proxy:
         proxies = {}
     else:
@@ -16,10 +22,12 @@ def test_proxy(proxy = None):
     try:
         res = requests.get(PING_URL, proxies=proxies, timeout=60)
         if (res.status_code == 200):
-            print('Proxy is working')
-            print('Response time', res.elapsed.total_seconds(), 'seconds')
+            # print('Proxy is working')
+            # print('Response time', res.elapsed.total_seconds(), 'seconds')
+            return { 'working': True, 'response_time': res.elapsed.total_seconds()}
     except Exception as err:
-        print('The proxy is down')
+        # print('The proxy is down')
+        return { 'working': False, 'response_time': None }
 
 def is_valid(proxy):
     RE_EXP = r'(^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9]))\:\d{2,5}'
@@ -32,30 +40,51 @@ def geo_db_reader(proxy):
     try:
         reader = maxminddb.open_database('dbip-country-lite-2020-02.mmdb')
         result = reader.get(ip)
-        print(result['continent']['names']['en'])
+        # print(result['continent']['names']['en'])
+        return { 'region': result['continent']['names']['en'] }
     except Exception as err:
-        print(traceback.format_exc())
+        # print(traceback.format_exc())
+        return { 'region': 'error' }
 
 def process_ip(proxy):
+    status = {'proxy': proxy }
     if is_valid(proxy):
-        geo_db_reader(proxy)
-        test_proxy(proxy)
+        status.update(geo_db_reader(proxy))
+        status.update(test_proxy(proxy))
+        return status
     else: 
         print('Invalid proxy:', proxy)
+        return { 'invalid': True, 'proxy': proxy}
+
+def file_handler(input_file):
+    data_string = ''
+    validated = []
+    with open(input_file, 'r') as myfile:
+        data_string = myfile.read()
+    # print(data_string)
+    if data_string:
+        data_string = data_string.replace('\n', ' ')
+        data_array = data_string.split(' ')
+        data_array = list(dict.fromkeys(data_array))
+        # print(f'Processing {len(data_array)} unique proxy...')
+        for item in data_array:
+            result = process_ip(item)
+            if 'invalid' not in result:
+               validated.append(result)
+        return validated
+
+def generate_report(result = []):
+    for item in result:
+        print(item)
 
 if __name__ == "__main__":
     input_args = argv[1:]
+
     for item in input_args:
         if item.endswith('.txt'):
             print('This is a text file')
-            data_string = ''
-            with open(item, 'r') as myfile:
-                data_string = myfile.read()
-            if data_string:
-                data_string = data_string.replace('\n', ' ')
-                data_array = data_string.split(' ')
-                data_array = list(dict.fromkeys(data_array))
-                for item in data_array:
-                    process_ip(item)
+            result = file_handler(item)
+            generate_report(result)
         else:
-            process_ip(item)
+            print('Processing')
+            print(process_ip(item))
---
+++ tests/test_app.py
@@ -0,0 +1,49 @@
+import unittest
+from unittest.mock import patch, call
+import app
+
+
+class Test(unittest.TestCase):
+
+    @patch('requests.get')
+    def test_test_proxy(self, mock_get):
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.elapsed.total_seconds.return_value = 1
+        self.assertEqual(app.test_proxy('207.246.78.234:80'), {'working': True, 'response_time': 1})
+        mock_get.side_effect = Exception('Connection Error')
+        self.assertEqual(app.test_proxy('207.246.78.234:80'), { 'working': False, 'response_time': None })
+
+    def test_is_valid(self):
+        self.assertTrue(app.is_valid('207.246.78.249:80'))
+        self.assertFalse(app.is_valid('207.246.78.299:80'))
+
+    def test_geo_db_reader(self):
+        self.assertEqual(app.geo_db_reader('207.246.78.2:80'), {'region': 'North America'})
+        self.assertEqual(app.geo_db_reader(''), {'region': 'error'})
+
+    @patch('app.geo_db_reader')
+    @patch('app.test_proxy')
+    def test_process_ip(self, mock_geo, mock_proxy):
+        proxy = '207.246.78.2:80'
+        mock_geo.return_value = {'region': 'Europe' }
+        mock_proxy.return_value = { 'working': True, 'proxy': proxy }
+        result = {
+            'region': 'Europe',
+            'working': True, 'proxy': proxy
+        }
+        self.assertEqual(app.process_ip(proxy), result)
+        self.assertEqual(app.process_ip('207.246.78.300:80'), {'invalid': True, 'proxy': '207.246.78.300:80'})
+
+    @patch('app.process_ip')
+    @patch('builtins.open', create=True)
+    def test_file_handler(self, mock_open, mock_ip):
+        mock_open.side_effect = [
+            unittest.mock.mock_open(read_data='3.86.30.40:3128').return_value
+        ]
+        mock_ip.return_value = {'region': 'Europe','working': True, 'proxy':'207.246.78.2:80'}
+        result = [{'region': 'Europe','working': True, 'proxy': '207.246.78.2:80'}]
+        self.assertEqual(app.file_handler('dev/null'), result)
+        
+
+if __name__ == "__main__":
+    unittest.main()
